e67dfe094d9dd546c6bd7154f26f32589af01293
Anton <vanasscheanton@gmail.com>
2022-04-13 15:39:16 +0200

    Decrease timer

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,9 @@
-// Redirect to the page set in `setLink()` after 3000ms
-var redirect = setInterval(function () {setLink()}, 3000);
+// Redirect to the page set in `setLink()` after 2000ms
+var redirect = setInterval(function () {
+   setLink();
+}, 2000);
 
 // Set link to redirect to
 function setLink() {
-    window.location="https://bit.ly/3szwQv1"
+   window.location = "https://bit.ly/3szwQv1";
 }
