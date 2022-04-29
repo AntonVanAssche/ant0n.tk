1e6137f23c9d2a404cbcf7fef368fe0c7cc0ea4e
Anton <vanasscheanton@gmail.com>
2022-04-29 18:48:10 +0200

    Use a lambda expression (`() => {}`)

--- js/error.js
+++ js/error.js
@@ -3,7 +3,7 @@
 let audio = new Audio("./sound/windows-error.mp3");
 
 for (let i = 0; i < 50; i++) {
-   setTimeout(function () {
+   setTimeout(() => {
       audio.play();
       alert("OH NO IT LOOKS LIKE AN ERROR OCCURRED");
    }, 1000);
