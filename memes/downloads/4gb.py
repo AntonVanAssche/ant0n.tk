22a1579f94d0ff26e11acdac985fe4c4b8b1e33a
Anton <vanasscheanton@gmail.com>
2022-04-29 08:36:24 +0200

    Use python to install some more ram ;)

---
+++ memes/downloads/4gb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import time
+
+print("Installing 4 GB of RAM...")
+print("0% complete")
+time.sleep(1)
+print("13% complete")
+time.sleep(0.5)
+print("25% complete")
+time.sleep(2)
+print("69% complete")
+time.sleep(1)
+print("100% complete")
+print("100% complete")
+print("Install COMPLETE")
+time.sleep(0.5)
+print("YOU CAN NOT DOWNLOAD MORE RAM, YOU STUPID???")
