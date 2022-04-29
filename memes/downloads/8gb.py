31affad99208c2597df187b950819172f2bdbabc
Anton <vanasscheanton@gmail.com>
2022-04-29 13:04:50 +0200

    Use sys.stdout.write to print progress

--- memes/downloads/8gb.py
+++ memes/downloads/8gb.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 import time
+import sys
 
-print("Installing 8 GB of RAM...")
-print("0% complete")
-time.sleep(1)
-print("13% complete")
-time.sleep(0.5)
-print("25% complete")
-time.sleep(2)
-print("69% complete")
-time.sleep(1)
-print("100% complete")
-print("100% complete")
-print("Install COMPLETE")
-time.sleep(0.5)
-print("YOU CAN NOT DOWNLOAD MORE RAM, YOU STUPID???")
+print('\033[92mInstalling 8 GB of RAM...\033[0m')
+for i in range(101):
+   sys.stdout.write('%d%% \033[92mcomplete\033[0m\r' % i)
+   sys.stdout.flush()
+   if i == 21 or i == 50 or i == 53 or i == 69:
+      time.sleep(1)
+   elif i == 99:
+      time.sleep(5)
+   else:
+      time.sleep(0.1)
+
+print('\033[92mInstallation complete\033[00m')
+print('\n')
+print('\033[91mYOU CAN NOT DOWNLOAD MORE RAM, YOU STUPID???\033[00m')
+print('\n')
