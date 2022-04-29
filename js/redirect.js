526586fa146db61defd3064543caac22ee6ee95f
Anton <vanasscheanton@gmail.com>
2022-04-29 11:46:35 +0200

    Reduce delay to 1 second

--- js/redirect.js
+++ js/redirect.js
@@ -9,10 +9,10 @@ const memes = [
 
 let randomMeme = Math.floor(Math.random() * memes.length);
 
-// Redirect to the page set in `setLink()` after 2000ms
+// Redirect to the page set in `setLink()` after 1000ms
 const redirect = setInterval(function () {
    setLink();
-}, 2000);
+}, 1000);
 
 // Set link to redirect to
 function setLink() {
