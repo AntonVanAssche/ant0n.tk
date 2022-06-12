ddea767880e1e4af07bf35c1599fcf43fb334ae7
Anton <vanasscheanton@gmail.com>
2022-06-12 18:53:22 +0200

    Typo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Define a list with all project pages.
-const list = [
+const projects = [
    { page: "/projects/bashpass.html", name: "BashPass" },
    { page: "/projects/dotfiles.html", name: "Dotfiles" },
    { page: "/projects/gnoti.html", name: "Gnoti" },
@@ -16,7 +16,7 @@ let darkMode = localStorage.getItem("darkMode")
 // Define the toggle button.
 const darkModeToggle = document.getElementById("darkmode-toggle")
 
-// Devine the toggle button icon.
+// Define the toggle button icon.
 const darkModeImg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`
 const lightModeImg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`
 
@@ -56,7 +56,7 @@ const windowLoaded = () => {
 $(window).load(() => {
    windowLoaded()
 
-   list.forEach(({ page, name }) => {
+   projects.forEach(({ page, name }) => {
       const liElement = document.createElement("li")
       const aElement = document.createElement("a")
       aElement.href = page
