8a6859019332995e605381097cd2ac830567127d
Anton <vanasscheanton@gmail.com>
2022-09-06 14:28:03 +0200

    Remove Update page

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const projects = [
    { page: "/projects/dotfiles.html", name: "Dotfiles" },
    { page: "/projects/gnoti.html", name: "Gnoti" },
    { page: "/projects/joke-bot.html", name: "Joke Bot" },
+   { page: "/projects/jredirect.html", name: "Jredirect" },
    { page: "/projects/music-bot.html", name: "Music Bot" },
    { page: "/projects/pythonstuff.html", name: "PythonStuff" },
    { page: "/projects/rpi-homeserver.html", name: "Raspberry Pi Home Server" },
