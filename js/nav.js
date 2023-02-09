61eb7ddbf0a588b4b70950708b99b2833020cd06
Anton <vanasscheanton@gmail.com>
2023-02-09 14:20:02 +0100

    Close the vam  menu when a option is clicked

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,24 @@
 "use strict";
 
+/**
+    * Function to execute when the menu button is clicked.
+    * Toggle the menu bar and nav.
+*/
 const menuOnClick = () => {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 };
+
+/**
+    * Function to ececute when a nav link is clicked.
+    * Close the menu and scroll to the section.
+    * @param {string} section: The section to scroll to.
+*/
+const navLinkOnClick = (section) => {
+    // Scroll to the section.
+    document.getElementById(section).scrollIntoView();
+
+    // Close the menu.
+    menuOnClick();
+}
